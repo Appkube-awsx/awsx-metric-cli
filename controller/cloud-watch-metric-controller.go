9ff1e49f45d87342a720d92e5f164732b96e27e0
ManojSynectiks <manoj.sharma@synectiks.com>
2023-11-30 17:22:25 +0530

    new api to process single query

--- controller/cloud-watch-metric-controller.go
+++ controller/cloud-watch-metric-controller.go
@@ -96,6 +96,63 @@ func GetMetricData(clientAuth *client.Auth, cloudWatchQueries string) (*cloudwat
 	return result, nil
 }
 
+func GetMetricDataWithSingleQuery(clientAuth *client.Auth, cloudWatchQueries string) (*cloudwatch.GetMetricDataOutput, error) {
+	var outerQuery OuterQuery
+	err := json.Unmarshal([]byte(cloudWatchQueries), &outerQuery)
+	if err != nil {
+		fmt.Println("Error parsing JSON input:", err)
+		return nil, err
+	}
+
+	// Create the metric queries dynamically
+	queries := make([]*cloudwatch.MetricDataQuery, 1)
+	for _, queryInput := range outerQuery.Query {
+		if queryInput.Dimensions == nil {
+			queryInput.Dimensions = make([]Dimension, 0)
+		}
+
+		query := &cloudwatch.MetricDataQuery{
+			Id:         aws.String(strings.ToLower(outerQuery.RefID)),
+			ReturnData: aws.Bool(true),
+			MetricStat: &cloudwatch.MetricStat{
+				Metric: &cloudwatch.Metric{
+					Namespace:  aws.String(queryInput.Namespace),
+					MetricName: aws.String(queryInput.MetricName),
+					Dimensions: buildDimensions(queryInput.Dimensions),
+				},
+				Period: aws.Int64(queryInput.Period),
+				Stat:   aws.String(queryInput.Stat),
+			},
+		}
+		queries[0] = query
+	}
+	
+	cloudWatchClient := client.GetClient(*clientAuth, client.CLOUDWATCH).(*cloudwatch.CloudWatch)
+
+	// Specify the request input with multiple queries
+	input := &cloudwatch.GetMetricDataInput{
+		MetricDataQueries: queries,
+		StartTime:         aws.Time(time.Now().Add(time.Duration(-1) * time.Hour)),
+		EndTime:           aws.Time(time.Now()),
+	}
+
+	// Make the request to CloudWatch
+	result, err := cloudWatchClient.GetMetricData(input)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	// Process the result
+	for _, metricDataResult := range result.MetricDataResults {
+		for i, timestamp := range metricDataResult.Timestamps {
+			fmt.Printf("Data for Metric at Timestamp %v: %f\n", *timestamp, *metricDataResult.Values[i])
+		}
+	}
+
+	return result, nil
+}
+
 // Existing buildDimensions function...
 
 func buildDimensions(dimensions []Dimension) []*cloudwatch.Dimension {
